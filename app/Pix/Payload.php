036940c7f9250f593eda79811d454ee4abe45d02
André Siqueira <siqueira.andre@gmail.com>
2021-12-20 07:31:47 -0300

    métodos iniciais da classe Payload

--- app/Pix/Payload.php
+++ app/Pix/Payload.php
@@ -7,4 +7,126 @@ namespace App\Pix;
 class Payload
 {
 
+    /**
+     * IDs do Payload do Pix
+     * @var string
+     */
+    const ID_PAYLOAD_FORMAT_INDICATOR = '00';
+    const ID_MERCHANT_ACCOUNT_INFORMATION = '26';
+    const ID_MERCHANT_ACCOUNT_INFORMATION_GUI = '00';
+    const ID_MERCHANT_ACCOUNT_INFORMATION_KEY = '01';
+    const ID_MERCHANT_ACCOUNT_INFORMATION_DESCRIPTION = '02';
+    const ID_MERCHANT_CATEGORY_CODE = '52';
+    const ID_TRANSACTION_CURRENCY = '53';
+    const ID_TRANSACTION_AMOUNT = '54';
+    const ID_COUNTRY_CODE = '58';
+    const ID_MERCHANT_NAME = '59';
+    const ID_MERCHANT_CITY = '60';
+    const ID_ADDITIONAL_DATA_FIELD_TEMPLATE = '62';
+    const ID_ADDITIONAL_DATA_FIELD_TEMPLATE_TXID = '05';
+    const ID_CRC16 = '63';
+
+    /**
+     * Chave pix
+     * @var string
+     */
+    private $pixKey;
+
+    /**
+     * Descrição do pagamento
+     * @var string
+     */
+    private $description;
+
+    /**
+     * Nome do titular da conta
+     * @var string
+     */
+    private $merchantName;
+
+    /**
+     * Cidade do titular da conta
+     * @var string
+     */
+    private $merchantCity;
+
+    /**
+     * ID da transação pix
+     * @var string
+     */
+    private $txid;
+
+    /**
+     * Valor da transação
+     * @var string
+     */
+    private $amount;
+
+
+    /**
+     * Método responsável por definir o valor do $pixkey
+     * @param string $pixKey
+     * @return $this
+     */
+    public function setPixKey($pixKey)
+    {
+        $this->pixKey = $pixKey;
+        return $this;
+    }
+
+    /**
+     * Método responsável por definir ao valor de $description
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Método responsável por definir ao valor de $merchantName
+     * @param string $merchantName
+     * @return $this
+     */
+    public function setMerchantName($merchantName)
+    {
+        $this->merchantName = $merchantName;
+        return $this;
+    }
+
+    /**
+     * Método responsável por definir ao valor de $merchantCity
+     * @param string $merchantCity
+     * @return $this
+     */
+    public function setMerchantCity($merchantCity)
+    {
+        $this->merchantCity = $merchantCity;
+        return $this;
+    }
+
+    /**
+     * Método responsável por definir ao valor de $txid
+     * @param string $txid
+     * @return $this
+     */
+    public function setTxid($txid)
+    {
+        $this->txid = $txid;
+        return $this;
+    }
+
+    /**
+     * Método responsável por definir ao valor de $amount
+     * @param float $amount
+     * @return $this
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = (string)number_format($amount,2,'.','');
+        return $this;
+    }
+
 }

3018e5b2fbc157052b0b47efba6656a264ccaadf
André Siqueira <siqueira.andre@gmail.com>
2021-12-22 01:06:47 -0300

    add funções do payload

--- app/Pix/Payload.php
+++ app/Pix/Payload.php
@@ -129,4 +129,98 @@ class Payload
         return $this;
     }
 
+
+    /**
+     * Responsável por retornar o valor completo de um objeto do payload
+     * @param string $id
+     * @param string $value
+
+     */
+    private function getValue($id, $value)
+    {
+        $size = str_pad(strlen($value),2,'0',STR_PAD_LEFT);
+        return $id.$size.$value;
+    }
+
+    /**
+     * Método responsável por retornar os valores completos da informação da conta
+     * @return string
+     */
+    private function getMerchantAccountInformation()
+    {
+        //DOMINIO DO BANCO
+        $gui = $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION,'br.gov.bcb.pix');
+
+        //CHAVE PIX
+        $key = $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION_KEY,$this->pixKey);
+
+        //DESCRIÇÃO DO PAGAMENTO
+        $descripton  = strlen($this->description) ? $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION_DESCRIPTION,$this->description) : '';
+
+        //VALOR COMPLETO DA CONTA
+        return $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION,$gui.$key.$descripton);
+    }
+
+    /**
+     * Método responsável por retornar os valores completos erar o código completo do payload Pix
+     * @return string
+     */
+    private function getAdditionalDataFieldTemplate()
+    {
+        //TXID
+        $txid = $this->getValue(self::ID_ADDITIONAL_DATA_FIELD_TEMPLATE_TXID,$this->txid);
+
+        //RETORNA O VALOR COMPLETO
+        return $this->getValue(self::ID_ADDITIONAL_DATA_FIELD_TEMPLATE,$txid);
+    }
+
+    /**
+     * Método responsável por gerar o código completo do payload Pix
+     * @return string
+     */
+    public function getPayload()
+    {
+        // CRIA O PAYLOAD
+        $payload = $this->getValue(self::ID_PAYLOAD_FORMAT_INDICATOR,'01').
+                   $this->getMerchantAccountInformation().
+                   $this->getValue(self::ID_MERCHANT_CATEGORY_CODE,'0000').
+                   $this->getValue(self::ID_TRANSACTION_CURRENCY,'986').
+                   $this->getValue(self::ID_TRANSACTION_AMOUNT,$this->amount).
+                   $this->getValue(self::ID_COUNTRY_CODE,'BR').
+                   $this->getValue(self::ID_MERCHANT_NAME,$this->merchantName).
+                   $this->getValue(self::ID_MERCHANT_CITY,$this->merchantCity).
+                   $this->getValue(self::ID_COUNTRY_CODE,'BR').
+                   $this->getAdditionalDataFieldTemplate();
+
+        //RETORNA O PAYLOAD + CRC16
+        return $payload . $this->getCRC16($payload);
+    }
+
+    /**
+     * Método responsável por calcular o valor da hash de validação do código pix
+     * @return string
+     */
+    private function getCRC16($payload) {
+        //ADICIONA DADOS GERAIS NO PAYLOAD
+        $payload .= self::ID_CRC16.'04';
+
+        //DADOS DEFINIDOS PELO BACEN
+        $polinomio = 0x1021;
+        $resultado = 0xFFFF;
+
+        //CHECKSUM
+        if (($length = strlen($payload)) > 0) {
+            for ($offset = 0; $offset < $length; $offset++) {
+                $resultado ^= (ord($payload[$offset]) << 8);
+                for ($bitwise = 0; $bitwise < 8; $bitwise++) {
+                    if (($resultado <<= 1) & 0x10000) $resultado ^= $polinomio;
+                    $resultado &= 0xFFFF;
+                }
+            }
+        }
+
+        //RETORNA CÓDIGO CRC16 DE 4 CARACTERES
+        return self::ID_CRC16.'04'.strtoupper(dechex($resultado));
+    }
+
 }
--- index.php
+++ index.php
@@ -2,3 +2,18 @@
 
 
 require __DIR__ . '/vendor/autoload.php';
+
+use \App\Pix\Payload;
+
+$obPayload = (new Payload)->setPixKey('32476592854')
+                          ->setDescription('Pagamento do pedido 123456')
+                          ->setMerchantName('Cleiton da silva')
+                          ->setMerchantCity('BOTUCATU')
+                          ->setAmount(100.00)
+                          ->setTxid('siqPix1234');
+
+$obPayloadQrCode = $obPayload->getPayload();
+
+echo "<pre>";
+print_r($obPayloadQrCode);
+echo "</pre>";

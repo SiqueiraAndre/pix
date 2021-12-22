39cd5daaaff42653f647645271731d0897337b0d
André Siqueira <siqueira.andre@gmail.com>
2021-12-22 01:19:28 -0300

    QrCode image base64

--- index.php
+++ index.php
@@ -4,16 +4,33 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use \App\Pix\Payload;
+use Mpdf\QrCode\QrCode;
+use Mpdf\QrCode\Output;
 
+
+//INSTANCIA PRINCIPAL DO PAYLOAD PIX
 $obPayload = (new Payload)->setPixKey('32476592854')
                           ->setDescription('Pagamento do pedido 123456')
-                          ->setMerchantName('Cleiton da silva')
+                          ->setMerchantName('Andre Siqueira')
                           ->setMerchantCity('BOTUCATU')
                           ->setAmount(100.00)
                           ->setTxid('siqPix1234');
 
-$obPayloadQrCode = $obPayload->getPayload();
+//CÓDIGO DE PAGAMENTO PIX
+$PayloadQrCode = $obPayload->getPayload();
 
-echo "<pre>";
-print_r($obPayloadQrCode);
-echo "</pre>";
+//QR CODE
+$obQrCode = new QrCode($PayloadQrCode);
+
+//IMAGEM DO QRCODE
+$image = (new Output\Png)->output($obQrCode,400);
+
+//header('Content-Type: image/png');
+//echo $image;
+?>
+<h1>QR CODE PIX</h1>
+<br>
+<img src="data:image/png;base64, <?=base64_encode($image)?>">
+<br><br>
+Código pix:<br>
+<strong><?=$PayloadQrCode?></strong>
